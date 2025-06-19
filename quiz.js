a81a36995f43ffd1ae6f597d0073070b16b2f01d
DuolDaniel <duolkuach100@gmail.com>
2025-06-19 10:29:48 +0300

    test quiz

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer() {
+    const correctAnswer = "4";
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
+    if (!selectedOption) {
+    document.getElementById("feedback").textContent = "Please select an answer.";
+    return;
+  }
+   const userAnswer = selectedOption.value;
+   if (userAnswer === correctAnswer) {
+    document.getElementById("feedback").textContent = "Correct! Well done.";
+  } else {
+    document.getElementById("feedback").textContent = "That's incorrect. Try again!";
+  }
+}
+const submitButton = document.getElementById("submit-answer");
+submitButton.addEventListener("click", checkAnswer);
